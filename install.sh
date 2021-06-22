4f06e6c1b536320fbf788f78583ec24c5842f325
KV Aditya Srivatsa <47175964+kvadityasrivatsa@users.noreply.github.com>
2021-06-22 18:22:39 +0530

    Update and rename requirements.txt to install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+python3 -m pip install Levenshtein
+python3 -m pip install flair
+python3 -m pip install spacy
+python3 -m spacy download en_core_web_sm
+python3 -m pip install nltk
+python3 -m nltk.downloader stopwords
+python3 -m pip install wget
+python3 -m pip install git+https://github.com/pytorch/fairseq@v0.10.0
+python3 -m pip install datasets@git+https://github.com/huggingface/datasets.git@a5e45816a72fbb20dc9122f88238ef8acba43ee3
+python3 -m pip install torch==1.7.0
+python3 -m pip install numpy==1.20.3
+python3 -m pip install git+https://github.com/feralvam/easse.git
